119cc5f3e1f8e14a1cf212eeba77dd389cefe8f0
Dimitri RODARIE <d.rodarie@gmail.com>
2024-08-14 13:27:56 +0200

    Update bsb_nest/connection.py
    Co-authored-by: Robin De Schepper <robin.deschepper93@gmail.com>

--- bsb_nest/connection.py
+++ bsb_nest/connection.py
@@ -97,9 +97,10 @@ class NestConnection(compose_nodes(NestConnectionSettings, ConnectionModel)):
         predicted_all_mem = (
             len(pre_nodes) * 8 * 2 + len(post_nodes) * 8 * 2 + len(cs) * 6 * 8 * (16 + 2)
         ) * MPI.get_size()
+        n_chunks = len(cs.get_local_chunks("out"))
         predicted_local_mem = (
-            (predicted_all_mem / len(cs.get_local_chunks("out")))
-            if len(cs.get_local_chunks("out")) > 0
+            (predicted_all_mem / n_chunks)
+            if n_chunks > 0
             else 0.0
         )
         if predicted_local_mem > avmem / 2:
