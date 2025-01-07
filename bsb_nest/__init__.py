833133c0538c5e2c81c9c76152999450dbe847b7
drodarie <drodarie@users.noreply.github.com>
2025-01-07 17:10:53 +0000

    docs: bump version: v4.3.1 → v4.3.2 [skip ci]

--- bsb_nest/__init__.py
+++ bsb_nest/__init__.py
@@ -9,4 +9,4 @@ from .adapter import NestAdapter
 from .simulation import NestSimulation
 
 __plugin__ = SimulationBackendPlugin(Simulation=NestSimulation, Adapter=NestAdapter)
-__version__ = "4.3.1"
+__version__ = "4.3.2"
